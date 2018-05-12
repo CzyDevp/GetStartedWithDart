b0824cc8c0df6f79885c27508c5e5cb373ddb3e5
NJ-Developer <navsingh191@gmail.com>
2018-05-12 13:11:46 -0400

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:GetStartedWithDart/GetStartedWithDart.dart' as GetStartedWithDart;
+
+main(List<String> arguments) {
+  print('Hello world: ${GetStartedWithDart.calculate()}!');
+}
---
+++ test/GetStartedWithDart_test.dart
@@ -0,0 +1,8 @@
+import 'package:GetStartedWithDart/GetStartedWithDart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
